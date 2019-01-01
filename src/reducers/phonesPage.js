299a82856629ffe8511df3fc4b9a6f0f17ec5d29
sashmen5 <sashmen5@gmail.com>
2019-01-01 18:18:06 +0200

    view phones reducer added

---
+++ src/reducers/phonesPage.js
@@ -0,0 +1,21 @@
+import * as R from 'ramda';
+import {
+	FETCH_PHONES_SUCCESS
+} from "../actions";
+
+
+const initialState = {
+	ids: []
+};
+
+export default (state = initialState, {type, payload}) => {
+	switch(type) {
+		case FETCH_PHONES_SUCCESS:
+			return R.merge(state,  {
+				ids: R.pluck('id', payload)
+			});
+
+		default:
+			return state;
+	}
+}
