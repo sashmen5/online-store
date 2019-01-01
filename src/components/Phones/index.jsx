4c83ba4690bf159594887d723d3025852ba6140c
sashmen5 <sashmen5@gmail.com>
2019-01-01 18:36:07 +0200

    Rendering of all phones added

--- src/components/Phones/index.jsx
+++ src/components/Phones/index.jsx
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPhones} from '../../actions';
-
+import {getPhones} from '../../selectors';
+import Link from "react-router/es/Link";
+import * as R from "ramda";
 
 class Phones extends Component {
 
@@ -9,17 +11,59 @@ class Phones extends Component {
     this.props.fetchPhones();
   }
 
+  renderPhone(phone, index) {
+    // TODO: separate to component Phone
+    const shortDescription = `${R.take(60, phone.description)}...`;
+
+    return (
+      <div className='col-sm-4 col-lg-4 col-md-4 book-list' key={index}>
+        <div className="thumbnail">
+          <img
+            className="img-thumbnail"
+            src={phone.image}
+            alt={phone.name}
+          />
+          <div className='caption'>
+            <h4 className='pull-right'>${phone.price}</h4>
+            <h4>
+              <Link to={`/phones/${phone.id}`}>
+                {phone.name}
+              </Link>
+            </h4>
+            <p>{shortDescription}</p>
+            <p className="itemButton">
+              <button className="btn btn-primary">Buy now!</button>
+              <Link
+                to={`/phones/${phone.id}`}
+                className='btn btn-default'
+              >
+                More info
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   render() {
+    const {phones} = this.props;
     return (
 			<div>
-				Phones
+				<div className="books row">
+          {phones.map((phone, index) => this.renderPhone(phone, index))}
+        </div>
 			</div>
     )
   }
 }
 
-const mapDispathToProps = {
+const mapStateToProps = state => ({
+  phones: getPhones(state)
+});
+
+const mapDispatchToProps = {
   fetchPhones
 };
 
-export default connect(null, mapDispathToProps)(Phones);
+export default connect(mapStateToProps, mapDispatchToProps)(Phones);
